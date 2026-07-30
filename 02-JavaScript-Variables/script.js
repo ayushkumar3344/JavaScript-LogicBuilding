82861cb1bd12050b0bd5827f702307aa31e27e50
user.name : "Ayush <kumarayush8117@gmail.com>
2026-07-30 19:48:19 +0530

    In this folder I Right Down the code how variables in javascript work step by step and describe the various scopes of blocks.

---
+++ 02-JavaScript-Variables/script.js
@@ -0,0 +1,57 @@
+// Variables in Javascript -->
+
+
+// ---------------------------------------->
+// 1 => (Var) Variable
+// Var is a Global Variable 
+
+// var age = 22;
+// var firstName = "Ayush Kumar";
+// console.log(firstName);
+
+
+// Naming conv.
+// first name 
+// first_name
+// FirstName
+// firstName (Preff) -> CammelCasing
+
+
+// ------------------------------------------>
+// 2 => (Let) Variable
+// Let is a Local Variable 
+
+
+// if(true){
+//     let fullName = "Ayush Kumar";
+//     fullName = "Shivam Dube";
+//     console.log(fullName);
+// }
+
+
+// ----------------------------------------->
+// 3 => (Const) Variable
+// Const is a Local Variable 
+// Constant variable ki value assign nahi ho sakti 
+
+
+// if(true){
+//     const classNumber = 45;
+//     console.log(classNumber);
+// }
+
+
+
+// var | let | const 
+
+// var - Global
+// Re-assign
+// Kabhi bhi acces kerlo
+
+// let - Local
+// Re-assign
+// Access sirf Khud ke Block ke Andhr
+
+// const - Local
+// Re-assign Nahi Kar Skte
+// Access sirf Khud ke Block ke Andhr
