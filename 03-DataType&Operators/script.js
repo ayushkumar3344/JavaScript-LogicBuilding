774a58230abeaac5b7b6374fe95a274b7cfbd98d
user.name : "Ayush <kumarayush8117@gmail.com>
2026-07-31 16:50:11 +0530

    Here i How how data Type and variables write down on our code and understanding about operators in javascript and how its work. And understanding the Deep Logical behaviour about JavaScript  in booleans. In this folder i write down the all concepts Step By Steps.

---
+++ 03-DataType&Operators/script.js
@@ -0,0 +1,84 @@
+// Data Types -->
+
+// // 1> Numbers -->
+// let num1 = 45.34;
+// let num2 = 60;
+
+
+
+// // 2> Strings -->
+// let firstName = "Ayush";
+// let lastName = "Kumar";
+
+
+
+// // 3> Booleans -->
+// let isLoggedIn = true;
+
+
+
+// // 4> NULL -->
+// let lastLoggedInDate = null;
+
+
+
+// // 5> Undefined -->
+// let className;
+
+
+
+// // 6> Objects -->
+// const personData = {
+//     fullName : "Ayush",
+//     lastName : "Kumar",
+//     age : 22,
+//     isLoggedIn : true,
+//     lastLoggedInDate : null,
+// }
+
+
+// --------------------------------------------------------------------------------------->
+
+
+// #OPERATORS IN JavaScript --->
+
+
+// 1> Boolean Sum Deep Logic Understanding -->  0 | 1
+// let isLoggedIn = true;
+
+// console.log(isLoggedIn + 20);
+
+// Note ->  Actually Boolean is a Number like --->
+// JS 1 -> True | 0 -> False
+// True | False
+
+// true --> 1
+// false --> 0
+
+
+
+// 2> String Deep Logic Understang -->  String '1' + '1';
+
+// string '1' + '1' --> String
+// string + number ---> String
+// string * number ---> Number
+
+// console.log('1' + "24");
+// console.log('1' + 56);
+// console.log('34' * 45);
+
+
+
+// Type of Operator -->
+
+// Example -->
+// console.log(typeof ("28" * 66));
+// console.log(typeof ("28" + "88"));
+
+
+
+
+
+
+
+
