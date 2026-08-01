89eccbc65aa39d72445bce51ea8422733414cd3d
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-01 17:46:09 +0530

    In this folder i write down deep understanding concept about how conditional statements work inside the javascript logic and understand how Switch Case code  create and how to use it.  and Know about Ternary Operator how it work and which is the syntax of a Ternary operator and which type of situations i use the ternary operator.

---
+++ 04-conditionalStatement/script.js
@@ -0,0 +1,83 @@
+// Conditional Statements :-->
+
+
+// 1> If Else Statement ----------------------------------->
+// const age = 80;
+
+// if(age >= 70){
+//     console.log("You can vote but u are our senior citizen");
+// } else if(age >= 18){
+//     console.log("You are eligible for voting.");
+// } else {
+//     console.log("Sorry, you're not eligible for voting");
+// }
+
+
+// Ternary Operator :-->
+
+
+// 1> Example --------------------->
+// let age = 20;
+
+// const respone =  age >= 18 ? "Eligible for voting" : "Not Eligible for voting"
+// console.log(respone);
+
+
+
+// Switch Case :-->
+
+
+
+// Example 1 ---------------------------->
+// const selectOption = 8;
+
+// switch(selectOption){
+//     case 1 : console.log("Number 1 Option is Clicked");
+//     break;
+//     case 2 : console.log("Number 2 Option is Selected");
+//     break;
+//     case 3 : console.log("Number 3 Option is Selected");
+//     break;
+//     case 4 : console.log("Number 4 Option is Selected");
+//     break;
+//     default : console.log("Invalid Option is Selected");
+// }
+
+
+// Example 2 ---------------------------->
+// const a = 30;
+// const operator = "9";
+// const b = 88;
+
+// switch(operator){
+//     case "+" : console.log("Value Added Successfully",a + b);
+//     break;
+//     case "-" : console.log("Value Subtract Successfully",a - b);
+//     break;
+//     case "*" : console.log("Value Multiply Successfully",a * b);
+//     break;
+//     case "/" : console.log("Value Divide Successfully",a / b);
+//     break;
+//     default : console.log("Only Valid this Four Operators - (+,-,*,/)");    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
