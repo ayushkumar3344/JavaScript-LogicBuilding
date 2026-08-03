499001f793ffb6ebacfa36040058eb7adbdca501
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-03 23:14:28 +0530

    778

---
+++ 04-conditionalStatement/practice.js
@@ -0,0 +1,75 @@
+// Practice Work Space --->
+
+
+// Question 1 :-> Write a conditional statement to check if a number is positive or negative.
+// Solution :->
+
+// let num = prompt("Enter a Number Here :");
+
+// if(num > 0 ){
+//     console.log('Positive Number');
+// } else if (num < 0){
+//     console.log('Negative Value');
+// } else {
+//     console.log("Zero");
+// }
+
+
+
+// Question 2 :-> Check whether a number is even or odd.
+// Solution :->
+
+// let num = Number(prompt('Enter Number here : '))
+
+// if(num % 2 == 0){
+//     console.log("Even Number");
+// } else if(num * 2 != 0){
+//     console.log("Odd Number");
+// }
+
+
+
+// Question 3 :-> Determine if a person is eligible to vote (age ≥ 18).
+// Solution :->
+
+// let age = Number(prompt('Enter your Age'));
+
+// if(age >= 18){
+//     console.log("Eligible");
+// } else {
+//     console.log("Not Eligible");
+// }
+
+
+
+// Question 4 :->Check if a student has passed (marks ≥ 40).
+// Solution :->
+
+// const marks = Number(prompt('Enter your marks here :'));
+
+// if(marks >= 40){
+//     console.log('Congraluation , You are passed');
+// } else {
+//     console.log('Sorry your result is Fail');
+// }
+
+
+
+// Question 5 :-> Find the greater of two numbers using an if-else statement.
+// Solution :->
+
+// let a = Number(prompt('Enter the value of A'));
+// let b = Number(prompt('Enter the value of B'));
+
+// if( a > b ){
+//     console.log(a, "Is grater than B");
+// } else {
+//     console.log(b, "is greater than A");    
+// }
+
+
+
+// Question 5 :-> Find the greater of two numbers using an if-else statement.
+// Solution :->
+
+
