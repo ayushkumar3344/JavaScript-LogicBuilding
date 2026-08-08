27c6d4d0444838841803e7784893ad08c06e397f
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-08 19:44:18 +0530

    Here I know how Logical Operator work behaviour inside the Javascript.

---
+++ 05-LogicalOperator/script.js
@@ -0,0 +1,48 @@
+// Logical Operator Notes ---------------------------->
+
+// 1> AND  --> All Condition must be true (&&)
+// 2> OR   --> Atleast One True (||)
+// 3> NOT  --> [True -> False] [False -> True] (!)
+
+
+
+
+
+// --------------------->
+// 1> AND Operator :
+
+// let age = 18;
+// let gender = "Male";
+
+// if(age >= 18 && gender == "Male"){
+//     console.log("Eligible for Licence");
+// } else {
+//     console.log("Not Eligible Try Next Time");
+// }
+
+
+
+// --------------------->
+// 2> OR Operator :
+
+// let age = 16;
+// let gender = "Male";
+
+// if(age >= 18 || gender == "Male"){
+//     console.log("Eligible for Licence");
+// } else {
+//     console.log("Not Eligible Try Next Time");
+// }
+
+
+
+// --------------------->
+// 3> NOT Operator :
+
+// let number = 8;
+
+// if(number % 2 != 0){
+//     console.log("Even");
+// } else {
+//     console.log("Odd"); 
+// }
