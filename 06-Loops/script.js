806c3097b77d117754da936e3565d542d50c453e
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-09 22:53:17 +0530

    Here i know how loops work inside the JavaScript Programming.

---
+++ 06-Loops/script.js
@@ -0,0 +1,70 @@
+// Loops In JavaScript ------------------>
+
+
+// 1> For Loop -----> If you know how many times to Loop.
+// 2> While Loop -----> 
+// 3> Do While Loop
+
+
+
+
+// 1> For Loop -->
+// for(let i=1 ; i<= 100 ; i++){
+//     console.log("Ayush",i);
+// }
+
+
+
+// 2> While Loop -->
+// let step = 0;
+// let home = 100;
+
+// while(step != home){
+//     step = step + 1;
+//     console.log(step);
+// }
+
+
+
+// 3> Do-While Loop -->
+// let i = 10;
+//  do{
+//     console.log(i);
+//     i++
+//  }while(i <= 50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
