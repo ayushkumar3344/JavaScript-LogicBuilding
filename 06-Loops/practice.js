4c75f4bac89fac74c8296869018023bb2c3aeef0
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-12 19:32:31 +0530

    Here i know the deep uses of Loop Step by Step

---
+++ 06-Loops/practice.js
@@ -0,0 +1,2 @@
+// Loop Practice Question Answers --->
+
--- 06-Loops/script.js
+++ 06-Loops/script.js
@@ -8,48 +8,68 @@
 
 
 
-// 1> For Loop -->
-// for(let i=1 ; i<= 100 ; i++){
-//     console.log("Ayush",i);
+// 1> For Loop --------------------------------------------------------------------------------
+// for(let i=1 ; i<=10000 ; i++){
+//     console.log("Ayush Kumar");
+// }
+
+// console.log("Loop Has Ended Now");
+
+// Example1 : Calculate Sum of 1 to 5?
+// Solution : 
+// let sum = 0;
+// let n = 10;
+// for(let i=1 ; i<=n ; i++){
+//     sum = sum + i;
+// }
+
+// console.log('Sum =',sum);
+
+
+
+// 2> While Loop --------------------------------------------------------------------------------
+// let i=1;
+// while(i<=10){
+//     console.log("i =",i);
+//     i++;
 // }
 
 
 
-// 2> While Loop -->
-// let step = 0;
-// let home = 100;
-
-// while(step != home){
-//     step = step + 1;
-//     console.log(step);
-// }
-
-
-
-// 3> Do-While Loop -->
-// let i = 10;
-//  do{
-//     console.log(i);
+// 3> Do-While Loop --------------------------------------------------------------------------------
+// let i=1;
+// do{
+//     console.log("Hello Ayush",i);
 //     i++
-//  }while(i <= 50)
+// }while(i<=10);
+// console.log("Loop Ended");
 
 
 
+// 4> For-of-Loop --------------------------------------------------------------------------------
+// let fullname = "AyushKumar";
+// let size = 0;
+
+// for(let i of fullname){
+//     console.log(i);
+//     size++;
+// }
+
+// console.log("Size of String is :",size);
 
 
 
+// 5> For-in-Loop --------------------------------------------------------------------------------
+// let student = {
+//     fullname : 'Ayush Kumar',
+//     age : 22,
+//     cgpa : 7.5,
+//     isPass : true,
+// }
 
-
-
-
-
-
-
-
-
-
-
-
+// for(let key in student){
+//     console.log("Keys =",key, " " ,"Value =",student[key]);
+// }
 
 
 
