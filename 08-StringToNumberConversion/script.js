c7116f10f2c0ea42b74c234e32a397d607cb4c72
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-17 23:52:47 +0530

    Here I How how strings behaviour Work in JavaScript

---
+++ 08-StringToNumberConversion/script.js
@@ -0,0 +1,142 @@
+// **************** #OPERATIONS *****************
+
+// let value = Number(prompt('Enter any number here :'));
+// let negValue = -value
+// console.log(negValue);
+
+
+
+// ------------------------------------------------------------------------>
+
+// Example 1 >>
+// let str1 = "Ayush"
+// let str2 = "kumar"
+
+// let str3 = str1 + " " + str2;
+// console.log(str3);
+
+
+
+// Example 2 >>
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((2 + 6) - 20 * -80);
+// console.log(true);
+// console.log();
+
+
+
+// Example 3 >>
+// let gameCounter = 100;
+// ++gameCounter
+// console.log(gameCounter);
+
+
+// Example 4 >>
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+
+
+// Example 5 >>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
