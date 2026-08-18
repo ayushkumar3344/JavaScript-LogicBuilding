4dde37670d3c05e10d6bc9048ba4c7c917b6215a
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-18 20:38:18 +0530

    here i now how comparison operator behaviour work in js

---
+++ 09-DataType-Comparison/script.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+
+
+// console.log("3" > 2);
+// console.log("2" < 3);
+
+
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+
+//   [ === ] <----- Strict Check
+
+// console.log("2" == 2);
+// console.log("2" === 2);
+
+
