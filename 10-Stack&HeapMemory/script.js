23b48ed66d82c2175d02733b71084a0db009fd09
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-22 14:48:53 +0530

    Here I understood the difference between how stack & heap memory work

---
+++ 10-Stack&HeapMemory/script.js
@@ -0,0 +1,64 @@
+// #MEMEORY --->
+
+// Stack Memory (Primitive) | Heap Memory (Non-Primitive)
+
+let yourName = "Ayush Kumar";
+let anotherName = yourName;
+anotherName = "Piyush Joshi"
+
+// console.log(yourName);
+// console.log(anotherName);
+
+
+
+// Heap Memory Example --> Object
+
+let userOne = {
+    emailID : 'kumarayush8117@gmail.com',
+    upiID : 'ayush@3400',
+}
+
+let userTwo = userOne
+
+userTwo.emailID = "hiteshSir0@gmail.com"
+
+console.log(userOne.emailID);
+console.log(userTwo.emailID);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
