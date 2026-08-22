7fd71afde2a33235056b0ded598f090b55e68066
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-22 16:40:37 +0530

    I know about the deep concept of String how its work and the how i find the usage of all string method function in detail

---
+++ 11-Strings/script.js
@@ -0,0 +1,64 @@
+// // #Strings In JavaScript ------------------>
+
+// let myName = "Ayush Kumar"
+// let age = 22
+
+// // console.log(myName + age + "" + " This is our Data Directory");
+// // console.log(`Hello , My Name is ${myName} & My Age is ${age} and I Am a MCA Student from PTU`);
+
+
+// // +++++ New Way To Declare String in our Code +++++
+
+
+// let country = new String("india");
+// console.log(country);
+// console.log(country.length);
+// console.log(country.toUpperCase());
+// console.log(country.indexOf('d'));
+// console.log(country.charAt(4));
+// console.log(country.padEnd(20,'Majhera'));
+
+
+// const newString = country.substring(0,3);
+// console.log(newString);
+
+// const anotherString = country.slice(-5,2);
+// console.log(anotherString);
+
+// const newStringOne = "    Ayush     "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+// const url = "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split";
+// console.log(url.replace("Web","----"));
+
+
+
+// const response = country.split('-');
+// console.log(response);
+
+
+// console.log(country.replaceAll("india","Goutam"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
