82f0cfeb05dd6394f12eaa798dbb7700f854da99
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-22 23:50:38 +0530

    JavaScript Practice Work --- Strings

---
+++ 11-Strings/practice.js
@@ -0,0 +1,21 @@
+// +++++++++++++ String Practice +++++++++++++++
+
+const college = "Vikram Batra college Palampur";
+
+const fullName = new String("Ayush Kumar");
+let sureName = "Thakur"
+
+
+// console.log(college.__proto__);
+
+// console.log(fullName.at(7));
+// console.log(fullName.charAt(3));
+// console.log(fullName.charCodeAt(9));
+// console.log(fullName.codePointAt(1));
+// console.log(fullName.concat(sureName));
+
+
+
+
+
+
