b414a9a917d66fdebb4473beddd838ce09e51818
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-23 16:27:30 +0530

    Here I Understand how methods of strings work in javascript strings

--- 11-Strings/practice.js
+++ 11-Strings/practice.js
@@ -1,9 +1,9 @@
 // +++++++++++++ String Practice +++++++++++++++
 
-const college = "Vikram Batra college Palampur";
+// const college = "Vikram Batra college Palampur";
 
-const fullName = new String("Ayush Kumar");
-let sureName = "Thakur"
+// const fullName = new String("Ayush Kumar");
+// let sureName = "Thakur"
 
 
 // console.log(college.__proto__);
@@ -13,6 +13,206 @@ let sureName = "Thakur"
 // console.log(fullName.charCodeAt(9));
 // console.log(fullName.codePointAt(1));
 // console.log(fullName.concat(sureName));
+// console.log(fullName.endsWith('Kumar'));
+
+
+
+// 1> length Method ---------->
+// const yourName = String(prompt('Enter anything here :'));
+// console.log(yourName.__proto__);
+// console.log(yourName.length);
+
+
+
+// 2> toUpperCase Method ---------->
+// const universityName = "Rayatbara University";
+// console.log(universityName.toUpperCase());
+
+
+
+// 3> toLowerCase Method ---------->
+// const fullName = "AYUSH KUMAR YADAV"
+// console.log(fullName.toLowerCase());
+
+
+
+// 4> trim Method ---------->
+// let yourName = "    priyanka Chopda";
+// console.log(yourName);
+// console.log(yourName.trim());
+
+
+
+// 5> includes Method ---------->
+// const gmailID = "kumarayush8117@gmail.com";
+// console.log(gmailID);
+// console.log(gmailID.includes('@') , gmailID.includes('.'));
+
+
+
+// 6> indexOf() Method ---------->
+// const passwordID = "ayush@26072004";
+// console.log(passwordID);
+// console.log(passwordID.indexOf('@'));
+// console.log(passwordID.indexOf('7'));
+
+
+
+// 7> lastIndexOf() Method ---------->
+// const cityName = "Palampur";
+// console.log(cityName);
+// console.log(cityName.lastIndexOf('p'));
+
+
+
+// 8> charAt() Method ---------->
+// const stateName = "Himachal Pradesh"
+// console.log(stateName);
+// console.log(stateName.charAt('1'));
+
+
+
+// 9> at() Method ---------->
+// const stateName = "Himachal Pradesh"
+// console.log(stateName);
+// console.log(stateName.at('-7'));
+
+
+
+// 10> slice() Method ---------->
+// const cityName = "Himachal Pradesh"
+// console.log(cityName);
+// console.log(cityName.slice('0','5'));
+
+
+
+// 11> substring() Method ---------->
+// const stateName = "Himachal Pradesh"
+// console.log(stateName);
+// console.log(stateName.substring('0','5'));
+
+
+
+// 12> replace() Method ---------->
+// const schoolName = "Shivalik School";
+// console.log(schoolName);
+// console.log(schoolName.replace('valik','walie'));
+
+
+
+// 13> replaceAll() Method ---------->
+// const fullName = "aditya";
+// console.log(fullName);
+// console.log(fullName.replaceAll('a','p'));
+
+
+
+// 14> split() Method ---------->
+// const sentence = "How are you brother?"
+// console.log(sentence);
+// console.log(sentence.split(' '));
+
+
+
+// 14> concat() Method ---------->
+// const firstName = "Ayush"
+// const lastName = " Kumar"
+// console.log(firstName);
+// console.log(firstName.concat(lastName));
+
+
+
+// 15> startsWith() Method ---------->
+// const courseName = "Masters in computer applications";
+// console.log(courseName);
+// console.log(courseName.startsWith('Masters'));
+
+
+
+// 16> endsWith() Method ---------->
+// const courseName = "Masters in computer applications";
+// console.log(courseName);
+// console.log(courseName.endsWith('tions'));
+
+
+
+// 17> repeat() Method ---------->
+// const courseName = "MCA ";
+// console.log(courseName.repeat(6));
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// #PRACTICE TASKS OF STRING ------------->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
