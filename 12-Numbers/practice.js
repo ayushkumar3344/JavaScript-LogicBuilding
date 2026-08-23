ab5fecca275bda9e8c987088c77b806017f4b6e2
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-23 22:51:27 +0530

    Few changes

---
+++ 12-Numbers/practice.js
@@ -0,0 +1,127 @@
+// ++++++++++++++ Numbers In JavaScript Practice Work ++++++++++++++
+
+
+// Easy — Questions 1–7 ---------------->
+
+
+// Example 1 : Convert String to Number
+// Take a string "250" and convert it into a number using a JavaScript Number method.
+// Solution : 
+// const score = Number('250');
+// console.log(score);
+
+
+// Example 2 : Check if a Value is an Integer
+// Given let num = 25, check whether the number is an integer.
+// Solution : 
+// const marks = 45;;
+// console.log(Number.isInteger(marks));
+
+
+// Example 3 : Check if a Value is NaN
+// Given let value = NaN, check whether it is NaN.
+// solution : 
+// const marks = "45"
+// console.log(Number.isInteger(marks));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 12-Numbers/script.js
+++ 12-Numbers/script.js
@@ -1,22 +1,22 @@
 // +++++++++++++++++++++ Numbers ++++++++++++++++++++++++
 
-const score = 400;
-// console.log(score);
+// const score = 400;
+// // console.log(score);
 
 
-const balance = new Number(100);
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+// const balance = new Number(100);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 
-const otherNumber = 23.94577;
-console.log(otherNumber);
-// console.log(otherNumber.toFixed(2));
-console.log(otherNumber.toPrecision(4));
+// const otherNumber = 23.94577;
+// console.log(otherNumber);
+// // console.log(otherNumber.toFixed(2));
+// console.log(otherNumber.toPrecision(4));
 
 
 
-const hundreds = 10000000;
-console.log(hundreds.toLocaleString('en-IN'));
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
 
 
 
