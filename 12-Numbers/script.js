04c8ca18517ffb46796f9cccf8be19b192930cf3
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-23 22:21:14 +0530

    Numbers in JavaScript Learning Journey

---
+++ 12-Numbers/script.js
@@ -0,0 +1,60 @@
+// +++++++++++++++++++++ Numbers ++++++++++++++++++++++++
+
+const score = 400;
+// console.log(score);
+
+
+const balance = new Number(100);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.94577;
+console.log(otherNumber);
+// console.log(otherNumber.toFixed(2));
+console.log(otherNumber.toPrecision(4));
+
+
+
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// +++++++++++++++++++++ Maths ++++++++++++++++++++++++
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
