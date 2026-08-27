d18e424179de5743635fbcb1494e2381db87a31f
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-27 15:29:32 +0530

    Static Method Compete successfully

--- 12-Numbers/script.js
+++ 12-Numbers/script.js
@@ -19,6 +19,75 @@
 // console.log(hundreds.toLocaleString('en-IN'));
 
 
+// -------> Static Numbers Methods <-------
+
+
+
+// 1> -------------- isFinite() Method;
+// const userNum = 497;
+// console.log(Number.isFinite(userNum));
+
+// const age = Number(prompt('Enter a Number here :'));
+// if(Number.isFinite(age)){
+//     console.log("yes it is a Number");
+// } else {
+//     console.log("its Not a Number");
+// }
+
+
+// 2> -------------- isInteger() Method;
+// const userNum = Number(prompt('Enter any Number here :'));
+// if(Number.isInteger(userNum)){
+//     console.log("Yes it is Valid Integer",userNum);
+// } else{
+//     console.log("its not a Integer",userNum);
+// }
+
+
+// 3> -------------- isNaN() Method;
+// const userNum = Number(prompt('Please enter a Number Here :'));
+// if(Number.isNaN(userNum)){
+//     console.log("NaN value");
+// } else {
+//     console.log("it is correct value");
+// }
+
+
+// 4> -------------- isSafeInteger() Method;
+// const userNum = Number(prompt('Enter any number here :'));
+// if(Number.isSafeInteger(userNum)){
+//     console.log("its Safe");
+// } else{
+//     console.log("not safe");
+// }
+
+
+// 5> -------------- isSafeInteger() Method;
+// const marksNum = 97.7;
+// console.log(String(marksNum).length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // +++++++++++++++++++++ Maths ++++++++++++++++++++++++
 
