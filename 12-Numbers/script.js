437af534c19ca33485b86ba9f93e06d62bae1fd5
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-29 22:04:36 +0530

    Understanding about math properties and methods

--- 12-Numbers/script.js
+++ 12-Numbers/script.js
@@ -77,22 +77,141 @@
 // +++++++++++++++++++++ Maths ++++++++++++++++++++++++
 
 
-console.log(Math);
-console.log(Math.abs(-32));  // It Help to convert Negative(-) Values to Positive(+)
-console.log(Math.round(3.7)); // It Remove after Decimal Values to Increase before Decimal Value. Start Working from after 5.
-console.log(Math.ceil(7.4)); // It Remove after Decimal Values to Increase before Decimal Value. Start Working from after 1.
-console.log(Math.floor(6.3)); // if you cange any value after decimal the value before decimal always same.
-console.log(Math.min(3,7,5,1,7,4,9,2)); // Its help to find the Smallest number given in Array;
-console.log(Math.max(3,7,5,1,7,4,9,2)); // Its help to find the Largest number given in Array;
+// console.log(Math);
+// console.log(Math.abs(-32));  // It Help to convert Negative(-) Values to Positive(+)
+// console.log(Math.round(3.7)); // It Remove after Decimal Values to Increase before Decimal Value. Start Working from after 5.
+// console.log(Math.ceil(7.4)); // It Remove after Decimal Values to Increase before Decimal Value. Start Working from after 1.
+// console.log(Math.floor(6.3)); // if you cange any value after decimal the value before decimal always same.
+// console.log(Math.min(3,7,5,1,7,4,9,2)); // Its help to find the Smallest number given in Array;
+// console.log(Math.max(3,7,5,1,7,4,9,2)); // Its help to find the Largest number given in Array;
 
 // Most Important Math Function Property is :
-console.log(Math.floor((Math.random() * 10) + 1));  // Random number Generator formula 
+// console.log(Math.floor((Math.random() * 10) + 1));  // Random number Generator formula 
 
 
-const min = 10;
-const max = 23;
+// const min = 10;
+// const max = 23;
 
-console.log(Math.floor(Math.random() * (max - min + 1) + min));  // Random number Generator formula 
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));  // Random number Generator formula 
+
+
+
+
+//  #PRACTICE MATH FUNTION METHODS --------------------------->
+
+
+// It help to show all functions properties in console.
+// console.log(Math);
+
+
+// 1> Math.round()
+// Sol ->
+// console.log(Math.round(6.5));
+// console.log(Math.round(-9.87));
+// console.log(Math.round(23.5300));
+
+
+// 2> Math.floor()
+// Sol ->
+// console.log(Math.floor(34.96));
+// console.log(Math.floor(34.26));
+// console.log(Math.floor(64.24));
+
+
+// 3> Math.ceil()
+// Sol ->
+// console.log(Math.ceil(3.3));
+// console.log(Math.ceil(-8.6));
+// console.log(Math.ceil(7.8));
+
+
+// 4> Math.trunc()
+// Sol ->
+// console.log(Math.trunc(45.6));
+// console.log(Math.trunc(67.36));
+// console.log(Math.trunc(-94.53));
+
+
+// 5> Math.random()
+// Sol ->
+// console.log(Math.floor(Math.random() * 30));
+
+
+// 6> Math.max()
+// Sol ->
+// console.log(Math.max(45,87,35,99,46,86));
+
+
+// 6> Math.min()
+// Sol ->
+// console.log(Math.min(45,87,35,99,46,86));
+
+
+// 7> Math.abs()
+// Sol ->
+// console.log(Math.abs(-685.7));
+// console.log(Math.abs(-34));
+// console.log(Math.abs(-83.3));
+
+
+// 8> Math.pow()
+// Sol ->
+// console.log(Math.pow(5,10));
+// console.log(Math.pow(3,5));
+// console.log(Math.pow(6,3));
+
+
+// 9> Math.sqrt()
+// Sol ->
+// console.log(Math.floor(Math.sqrt(25)));
+
+
+// 10> Math.cbrt()
+// Sol ->
+// console.log(Math.cbrt(55));
+
+
+// 11> Math.sign()
+// Sol ->
+// console.log(Math.sign(768));
+// console.log(Math.sign(-768));
+
+
+// 12> Math.log()
+// Sol ->
+// console.log(Math.log(24));
+
+
+// 13> Math.PI
+// Sol ->
+// console.log(Math.PI);
+
+
+// 14> Math.E
+// Sol ->
+// console.log(Math.E);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
