c3a868eb46a94ffe73d5bcca1397accaae15bf55
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-30 17:30:37 +0530

    Now i understand how live date object and use in our projects

---
+++ 13-Date&Time/script.js
@@ -0,0 +1,97 @@
+// #Dates & Time -------------------------------------------------->
+
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+
+// This way you customized your date ------------>
+// let myCreatedDate = new Date(2026,11,22);
+// let myCreatedDate = new Date(2026,11,22,10,15);
+// let myCreatedDate = new Date("02-25-2026");
+// console.log(myCreatedDate);
+
+// let myTime = Date.now();
+// console.log(myTime);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+// let todayDate = new Date();
+// // console.log(todayDate.getMinutes());
+// console.log(todayDate.toString());
+
+
+// console.log(todayDate.toLocaleString('Local',{
+//     weekday : "long",
+// }));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
