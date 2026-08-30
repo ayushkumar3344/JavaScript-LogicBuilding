5786dc234af936577ecc7c5928493217c91bda3e
user.name : "Ayush <kumarayush8117@gmail.com>
2026-08-30 18:17:04 +0530

    Date Object all get method understand deeply

---
+++ 13-Date&Time/practice.js
@@ -0,0 +1,123 @@
+// #Practice Notes ------------->
+
+// -----------------------------------------------------------------------------------
+// const todayDate = new Date();
+// console.log(todayDate);
+
+// Using all  Get Methods of Date -->
+
+// 1. getDate();
+// sol ->
+// console.log(todayDate.getDate());
+
+
+// 2. getDay();
+// sol ->
+// console.log(todayDate.getDay());
+
+
+// 3. getMonth();
+// sol ->
+// console.log(todayDate.getMonth() + 1);
+
+
+// 4. getFullYear();
+// sol ->
+// console.log(todayDate.getFullYear());
+
+
+// 5. getHours();
+// sol ->
+// console.log(todayDate.getHours());
+
+
+// 6. getMinutes();
+// sol ->
+// console.log(todayDate.getMinutes());
+
+
+// 7. getSeconds();
+// sol ->
+// console.log(todayDate.getSeconds());
+
+
+// 8. getMilliseconds()
+// sol ->
+// console.log(todayDate.getMilliseconds());
+
+
+// 9. getTime()
+// sol ->
+// console.log(todayDate.getTime());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
