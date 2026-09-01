4a10386c248c841842c5d113ab8fc2f4bb39f225
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-01 16:29:15 +0530

    In this folder I noted about how Array Use and Control in Javascript

---
+++ 14-Array/script.js
@@ -0,0 +1,58 @@
+// ++++++++++ Array In Javascript +++++++++++
+
+
+
+
+// #ARRAY :-
+
+// const myArr = [0, 1, 2, 3, 4, 5, 6]
+// const myHeroes = ["IronMan", "SaktiMan", "SpiderMan", "HanuMan"]
+// const myArr2 = new Array(4, 7, 3, 3, 8, 3)
+
+// console.log(myArr[0]);
+
+
+
+// #ARRAY METHODS :-
+
+// const myArr = [0, 1, 2, 3, 4, 5, 6]
+
+// myArr.push(7, 8, 9, 10);
+// console.log("Result is :", myArr);
+// console.log(myArr.length);
+
+
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(0);   // 0 Added on Starting
+// myArr.shift()   // 0 Removed
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+// slice | splice :-
+
+// console.log("A",myArr);
+// console.log(myn1);
+// console.log("B",myArr);
+
+// const myn1 = myArr.slice(1,4);
+// const myn2 = myArr.splice(1,4)
+// console.log(myn1);
+// console.log(myn2);
+
+
+
+
+
+
+
+
+ 
