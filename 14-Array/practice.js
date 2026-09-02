0d88d41a9bfdd5c3bcc166dc2d5b643806924ec9
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-02 13:37:55 +0530

    Besic Examples method Of Array Solved Successfully

--- 14-Array/practice.js
+++ 14-Array/practice.js
@@ -1,14 +1,48 @@
 // Practice Tssk Questions -->
 
 
+// 🟢 Beginner ------>
 
 
+// Q1> Create an array of 5 numbers. Add a new number to the end of the array and print the updated array.
+// Sol :
+// const userArray = [0, 1, 2, 3, 4, 5];
+// const output = userArray.push(6, 7, 8);
+// console.log(userArray);
 
 
+// Q2> Create an array of 5 fruits. Add "Mango" to the beginning of the array.
+// Sol :
+// const markitFruits = ['Apple', 'Banana', 'Graphes', 'Orange', 'PineApple'];
+// const output = markitFruits.unshift('Mango');
+// console.log(markitFruits);
 
 
+// Q3> Given the array:
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+// Remove the last fruit and print the updated array.
+// Sol :
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+// let output = fruits.pop();
+// console.log(fruits);
 
 
+// Q4> Given the array:
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+// Given the same array, remove the first fruit and print the updated array.
+// Sol :
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+// let output = fruits.shift();
+// console.log(fruits);
+
+
+// Q5> Given:
+// let numbers = [10, 20, 30, 40, 50];
+// Find the largest number in the array.
+// Sol :
+// let numbers = [10, 45, 23, 89, 12, 67];
+// let largest = Math.max(...numbers);
+// console.log(largest);
 
 
 
