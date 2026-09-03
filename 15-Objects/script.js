721ccae453e5ce12e0d92bae732a786ebab72b3c
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-03 22:31:03 +0530

    understanding objects in dept

--- 15-Objects/script.js
+++ 15-Objects/script.js
@@ -3,14 +3,14 @@
 // 1> Singleton.
 // 2> Object Literals.
 
-const jsUser = {
-    fullName : "Ayush",
-    age : 20,
-    email : 'kumarayush8117@gmail.com',
-    courseName : 'MCA',
-    isLoggedIn : true,
-    lastLogginDays : ['Monday','Saturday'],
-}
+// const jsUser = {
+//     fullName : "Ayush",
+//     age : 20,
+//     email : 'kumarayush8117@gmail.com',
+//     courseName : 'MCA',
+//     isLoggedIn : true,
+//     lastLogginDays : ['Monday','Saturday'],
+// }
 
 // console.log("Current Age of User is :", jsUser.age);   // Old Method To access Object Values.
 // console.log("User Email Is :", jsUser["email"]);    // New Way To Acces Object Values.
@@ -18,24 +18,48 @@ const jsUser = {
 // console.log("Course :", jsUser['courseName']);
 
 
-jsUser.greeting = function(){
-    console.log('Hello JS User');
-}
+// jsUser.greeting = function(){
+//     console.log('Hello JS User');
+// }
 
-jsUser.greeting2 = function(){
-    console.log(`Hello Coders, ${this.fullName}`);
-}
+// jsUser.greeting2 = function(){
+//     console.log(`Hello Coders, ${this.fullName}`);
+// }
 
-console.log(jsUser.greeting2());
-
-
+// console.log(jsUser.greeting2());
 
 
 
 
 
+// ------------ #OBJECT VIDEO 2 ------------
 
 
+
+// Here I understand how to create object in Singleton Way --->
+
+// const tinder = new Object();
+// const tinder = {}
+
+// tinder.fullName = 'Ayush Kuamr'
+// tinder.age = 23
+// tinder.isLoggedIn = true
+// tinder.course = "MCA(Masters in Computer Applications)"
+
+// console.log(tinder);
+
+// const regularUser = {
+//     email : "kumarayush8117@gmail.com",
+//     fullName : {
+//         userFullName : {
+//             firstName : "Ayush",
+//             lastName : "kumar",
+//             age : 25,
+//         }
+//     }
+// }
+
+// console.log(regularUser.fullName?.userFullName.firstName);
 
 
 
