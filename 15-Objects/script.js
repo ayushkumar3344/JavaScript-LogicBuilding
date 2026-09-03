e0de917a676efb50746a60a885b4c2e5cee391d6
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-03 20:10:30 +0530

    Here i now how access object values

---
+++ 15-Objects/script.js
@@ -0,0 +1,114 @@
+// ---> #OBJECTS <---
+
+// 1> Singleton.
+// 2> Object Literals.
+
+const jsUser = {
+    fullName : "Ayush",
+    age : 20,
+    email : 'kumarayush8117@gmail.com',
+    courseName : 'MCA',
+    isLoggedIn : true,
+    lastLogginDays : ['Monday','Saturday'],
+}
+
+// console.log("Current Age of User is :", jsUser.age);   // Old Method To access Object Values.
+// console.log("User Email Is :", jsUser["email"]);    // New Way To Acces Object Values.
+// jsUser.courseName = "BBA";
+// console.log("Course :", jsUser['courseName']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
