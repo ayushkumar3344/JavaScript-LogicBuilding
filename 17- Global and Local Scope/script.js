0fb738b6b607eef37a93c424cf4e5b8ee79bc90c
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-10 09:16:59 +0530

    Variables complete

--- 17- Global and Local Scope/script.js
+++ 17- Global and Local Scope/script.js
@@ -23,22 +23,22 @@
 
 
 
-let a = 20
+// let a = 20
 
-if(true){
-   let a = 10;
-   const b = 20;
-//    var c = 30;
-   console.log("Value of A is :", a);  //Local Scope
+// if(true){
+//    let a = 10;
+//    const b = 20;
+// //    var c = 30;
+//    console.log("Value of A is :", a);  //Local Scope
    
-}
+// }
 
-console.log("A is : ",a); // Global Scope
+// console.log("A is : ",a); // Global Scope
 
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
 
 
 
