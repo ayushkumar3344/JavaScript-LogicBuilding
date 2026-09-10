f455eb28ecc727bff3a8f2fd4e717c2de88fc06e
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-10 09:09:43 +0530

    Understanding deep

--- 17- Global and Local Scope/script.js
+++ 17- Global and Local Scope/script.js
@@ -1,7 +1,25 @@
 // Javascript Scopes -->
 
+// let a = 10;
+// const b = 20;
+// var c = 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 
+// var c = 30;
+
+// if(true){
+//    let a = 10;
+//    const b = 20;
+// //    var c = 30;
+// }
+
+// // console.log(a);
+// // console.log(b);
+// console.log(c);
 
 
 
