1bc79c3a458d6f137c971d59f771fcd5ae618608
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-11 09:07:13 +0530

    Four Examples Solved

--- 16-Functions/practice.js
+++ 16-Functions/practice.js
@@ -2,20 +2,51 @@
 
 
 
+// 🟢 Basic Function Tasks  ---> 
 
 
+// Q1 : Greeting Function
+// Create a function that takes a person's name as a parameter and prints:
+// Hello, Ayush!
+// Sol : 
+// function person(name){
+//     console.log(`Hello, ${name}`);
+// }
+
+// person(prompt('Enter Your Name'));
 
 
+// Q2 : Add Two Numbers
+// Create a function that accepts two numbers and returns their sum.
+// Sol : 
+// function sum(num1, num2){
+//     return num1 + num2;
+// }
+
+// const output = sum(34, 67);
+// console.log("Total is :", output);
 
 
+// Q3 : Subtract Two Numbers
+// Create a function that accepts two numbers and returns their difference.
+// Sol :
+// function sum(num1, num2){
+//     return num1 - num2;
+// }
+
+// const output = sum(56, 76);
+// console.log("Output is :", output);
 
 
+// Q4 : Find Square
+// Create a function that accepts a number and returns its square.
+// Sol :
+function sum(getNum){
+    return getNum * 2
+}
 
-
-
-
-
-
+const output = sum(5);
+console.log("Output is :", output);
 
 
 
