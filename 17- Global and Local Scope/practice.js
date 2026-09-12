7f78428df785932b0e61200eb44999a594a9dfa9
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-12 15:04:44 +0530

    All Functions Revision Done

--- 17- Global and Local Scope/practice.js
+++ 17- Global and Local Scope/practice.js
@@ -1,2 +1,62 @@
 // Practice Work -->
 
+
+
+
+// Example 1 -->
+
+// function one(){
+
+//     const fullName = "Ayush Kumar";
+//     const age = 32;
+
+//     function two(){
+
+//         const itemName = "Ice Cream";
+//         const prize = 299;
+//         console.log(`Hye : My Name is ${fullName} & My Current Age is ${age}`);
+        
+
+//     }
+    
+//     two()
+
+// }
+
+// one()
+
+
+
+
+
+// Example 2 -->
+
+// if(true){
+
+//     const gameNmae = 'FootBall';
+//     const round = 'Three'
+
+//     if(true){
+
+//         const playerName = 'Mukesh Gupta';
+//         const currentAge = 25;
+//         console.log(`The game name is ${gameNmae} and the round i will play of this game is ${round}`);
+
+//     }
+
+//     // console.log(`Player is : ${playerName} & player age is ${currentAge}`);
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
