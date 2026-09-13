5243f1c5644a5cbe15e7544d11f0a3b65e0c5eb2
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-13 19:13:17 +0530

    Function Concept Done

--- 16-Functions/practice.js
+++ 16-Functions/practice.js
@@ -232,27 +232,25 @@
 // Q4> Count Vowels
 // Create a function countVowels(str) that returns the total number of vowels (a, e, i, o, u) in a string.
 // Solution :
-function countVowels(str) {
-    let count = 0;
+// function countVowels(str) {
+//     let count = 0;
 
-    for (let char of str.toLowerCase()) {
-        if (
-            char === "a" ||
-            char === "e" ||
-            char === "i" ||
-            char === "o" ||
-            char === "u"
-        ) {
-            count++;
-        }
-    }
+//     for (let char of str.toLowerCase()) {
+//         if (
+//             char === "a" ||
+//             char === "e" ||
+//             char === "i" ||
+//             char === "o" ||
+//             char === "u"
+//         ) {
+//             count++;
+//         }
+//     }
 
-    return count;
-}
+//     return count;
+// }
 
-console.log(countVowels("Education"));
-
-
+// console.log(countVowels("Education"));
 
 
 
