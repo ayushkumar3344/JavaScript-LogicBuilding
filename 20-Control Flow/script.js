d4f7a55e7feaa922322b7eb2f412a9d97adfbb62
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-14 17:29:52 +0530

    Understand if in control Flow

---
+++ 20-Control Flow/script.js
@@ -0,0 +1,124 @@
+// Control Flow In JavaScript Notes :-->
+
+
+// if 
+// if(true){
+//     console.log("Code Execute");
+// }
+// if(false){
+//     console.log("Code Not Execute");
+// }
+
+
+// // Example ->
+// const userLoggedIn = true;
+// const temperature = 41;
+
+// if(temperature <= 50){
+//     console.log('Code Executed');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
