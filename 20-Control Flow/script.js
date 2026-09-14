1db5c8bbada49a1aab9d15071c1a270933a45a29
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-14 17:56:27 +0530

    Deeply Know about how Control Flow Work Inside Javascript

--- 20-Control Flow/script.js
+++ 20-Control Flow/script.js
@@ -20,9 +20,51 @@
 
 
 
+// If-Else in Control Flow ------>
+// const score = 200;
+
+// if( score > 100 ){
+//     let power = "Fly";
+//     console.log(`Power is : ${power}`);
+// }
+
+// console.log(`Power now is : ${power}`); // it is not access the local variable declared values.
 
 
+// const balance = 7893;
 
+// if(balance < 500){
+//     console.log(`your balance is less than 500`);
+// } else if(balance < 700){
+//     console.log(`your balance is less than 700`);
+// } else if(balance < 800){
+//     console.log(`your balance is less than 800`);
+// } else if(balance < 900){
+//     console.log(`your balance is less than 900`);
+// } else if(balance < 1200){
+//     console.log(`your balance is less than 1200`);
+// } else {
+//     console.log(`your current balance is ${balance}🪙`);
+// }
+
+
+// Example 2 -->
+// const isLoggedIn = true;
+// const dabitCard = true;
+
+// if(isLoggedIn && dabitCard){
+//     console.log("Yes!! You can buy our course");
+// }
+
+
+// Example 3 -->
+// const isLoggedInWithGoogle = true;
+// const isLoggedInWithfacebook = false;
+// const isLoggedInWithGuestAccount = false;
+
+// if(isLoggedInWithfacebook || isLoggedInWithGuestAccount || isLoggedInWithGoogle){
+//     console.log("You Logged In Successfully");
+// }
 
 
 
