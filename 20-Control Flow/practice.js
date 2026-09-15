97378ee9d60855dad64c6b62021166f79cf7132a
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-15 17:40:21 +0530

    First Hard Level Example Solved Successfully

--- 20-Control Flow/practice.js
+++ 20-Control Flow/practice.js
@@ -3,7 +3,36 @@
 
 // 🔥 15 Hard Conditional Statement Tasks --->
 
+// Q1 : Student Grade + Scholarship
+// Take marks and family income.
+// Grade:
+// 90–100 → A
+// 80–89 → B
+// 70–79 → C
+// 60–69 → D
+// Below 60 → F
+// If marks ≥ 85 and income < ₹3,00,000, print "Eligible for Scholarship".
+// Sol ::
+// const totalMarks = Number(prompt('Enter Your Current Marks Here (?/100) :'));
+// const familyIncome = Number(prompt('Enter Your Current Annual Family Income :'));
 
+// if(totalMarks >= 90 && totalMarks <= 100){
+//     console.log(`${totalMarks} : Grade A`);
+// } else if(totalMarks >= 80 && totalMarks <= 89){
+//     console.log(`${totalMarks} : Grade B`);
+// } else if(totalMarks >= 70 && totalMarks <= 79){
+//     console.log(`${totalMarks} : Grade C`);
+// } else if(totalMarks >= 60 && totalMarks <= 69){
+//     console.log(`${totalMarks} : Grade D`);
+// } else{
+//     console.log(`${totalMarks} : Result Fail`);
+// }
+
+// if(familyIncome <= 300000){
+//     console.log(`Yes you are eligible for Scholership`);
+// } else {
+//     console.log(`Sorry you are not eligible for scholership`);
+// }
 
 
 
