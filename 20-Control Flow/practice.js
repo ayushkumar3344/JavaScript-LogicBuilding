d15c6a1945a35a0396624c74ea49218801b3fbbd
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-15 18:07:57 +0530

    second example solved

--- 20-Control Flow/practice.js
+++ 20-Control Flow/practice.js
@@ -36,7 +36,29 @@
 
 
 
+// Q2 : Leap Year + Century
+// Take a year.
+// Determine whether it is a leap year.
+// Additionally, print whether it is a Century Year or Non-Century Year.
+// Example: 2000 → Leap Year, Century Year.
+// Sol ::
+// const year = 2000;
 
+// if(year % 400 === 0){
+//     console.log("Leap Year");
+// } else if(year % 100 === 0){
+//     console.log("Not a Leap Year");
+// } else if(year % 4 === 0){
+//     console.log("Leap year");
+// } else{
+//     console.log("Not a Leap Year");
+// }
+
+// if(year % 100 === 0){
+//     console.log("Century Year");
+// } else {
+//     console.log("Non-Century year");
+// }
 
 
 
