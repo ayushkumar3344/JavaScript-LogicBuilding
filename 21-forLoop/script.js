63eca5c51294ced64c61140df4204c05b1c88c6b
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-19 21:50:29 +0530

    Here I Understand how Array Work with loops

--- 21-forLoop/script.js
+++ 21-forLoop/script.js
@@ -23,12 +23,15 @@
 // }
 
 
+// --------->
 
+// let myArray = ['flash', 'batman', 'superman']
 
-
-
-
-
+// for (let index = 0; index < myArray.length; index++) {
+    
+//     const element = myArray[index];
+//     console.log(`Elements are : ${element}`);
+// }
 
 
 
