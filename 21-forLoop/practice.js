d5e2b352ebe745d28b69722d1041d4ae0c6a183d
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-20 22:31:45 +0530

    3 Example Solved Successfully of For Loop

--- 21-forLoop/practice.js
+++ 21-forLoop/practice.js
@@ -7,15 +7,30 @@
 // Q1 > Print Numbers
 // Print all numbers from 1 to 50 using a for loop.
 // Sol : 
+// for(let i=0; i<=50; i++){
+//     console.log('Value of i is :', i);
+// }
 
 
+// Q2 > Even Numbers
+// Print all even numbers between 1 and 100.
+// Sol : 
+// for (let i = 0; i < 100; i++) {
+//     if(i % 2 === 0){
+//         console.log(`Even Numbers Are : ${i}`);
+//     }
+// }
 
 
+// Q3 > Sum of Numbers
+// Calculate the sum of all numbers from 1 to 100.
+// Sol : 
+// let sum = 0;
 
-
-
-
-
+// for(let i=0 ; i<=100 ; i++){
+//     sum = sum + i;
+//     console.log("Total is", sum);
+// }
 
 
 
