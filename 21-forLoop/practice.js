70fe08cb01a9d84552783bb30f42121eb35c6d1d
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-20 22:11:14 +0530

    Level 1 of Solving For Loop problem solving questions

---
+++ 21-forLoop/practice.js
@@ -0,0 +1,56 @@
+// #Practice Examples --->
+
+
+// 🟢 Level 1 — Basic Logic
+
+
+// Q1 > Print Numbers
+// Print all numbers from 1 to 50 using a for loop.
+// Sol : 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
