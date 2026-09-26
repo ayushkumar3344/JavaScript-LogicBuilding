7ee863504fb0fefe2b9aa066779cafb777f6dc80
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-26 17:43:20 +0530

    here i complete while Loop Properly

--- 22-While & Do-While/script.js
+++ 22-While & Do-While/script.js
@@ -1,8 +1,25 @@
 // Learning Area --->
 
 
+// 1> While Loop ----->
 
+// let index = 0;
 
+// while(index <= 10){
+//     console.log(`Value of Index is ${index}`);
+//     index = index + 2;
+// }
+
+// While Loop in Array :
+
+// let superMan = ['Saktiman', 'SpiderMan', 'HanuMan', 'Thor', 'Flash'];
+
+// let arr = 0;
+
+// while(arr < superMan.length){
+//     console.log(`Values Are : ${superMan[arr]}`);
+//     arr++;
+// }
 
 
 
