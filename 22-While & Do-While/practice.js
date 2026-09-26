7afe6ca1432222eb534ff92782f29c90a724f6f1
user.name : "Ayush <kumarayush8117@gmail.com>
2026-09-26 21:03:37 +0530

    Here I Solve a four example of While Loop

---
+++ 22-While & Do-While/practice.js
@@ -0,0 +1,244 @@
+// Practice Work Start Here ----->
+
+
+// ---------------------->
+// Beginner Level -->
+
+
+// Q1: Print Numbers from 1 to 10
+// Write a while loop to print numbers from 1 to 10.
+// Sol :
+// let i = 1;
+
+// while(i <= 10){
+//     console.log(`Value is ${i}`);
+//     i++;
+// }
+
+
+// Q2: Print Even Numbers
+// Print all even numbers from 1 to 20 using a while loop.
+// Sol :
+// let i = 1;
+
+// while(i <= 20){
+//     if(i % 2 === 0){
+//         console.log(`Values are ${i}`);
+//     }
+//     i++;
+// }
+
+
+// Q3: Print Odd Numbers
+// Print all odd numbers from 1 to 20 using a while loop.
+// Sol :
+// let i = 1;
+
+// while(i <= 20){
+//     if(i % 2 !== 0){
+//         console.log(`Odd Numbers Are : ${i}`);
+//     }
+//     i++;
+// }
+
+
+// Q4: Print Numbers in Reverse
+// Print numbers from 10 down to 1.
+// Sol :
+// let i = 10;
+
+// while(i >= 1){
+//     console.log(`Reverse Values are : ${i}`);
+//     i--;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
